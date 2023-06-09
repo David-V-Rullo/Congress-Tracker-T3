d0f8b371246a1e36119eda3d7dde721f5c99f67e
David-V-Rullo <d.v.rullo@gmail.com>
2023-06-09 15:58:19 -0400

    Updated router, working on structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import { SignInButton, useUser } from "@clerk/nextjs";
-import { Sign } from "crypto";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -18,25 +15,23 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Congressional Bill Tracker
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <SignInButton />
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
+          <div>
+            {user.isSignedIn && <SignOutButton />}
+            {!user.isSignedIn && <SignInButton />}
           </div>
+
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">HI</p>
+            <p className="text-2xl text-white">
+              {data?.map((post) => (
+                <div key={post.id}>
+                  <p>{post.title}</p>
+                  <p>{post.content}</p>
+                </div>
+              ))}
+            </p>
           </div>
         </div>
       </main>
